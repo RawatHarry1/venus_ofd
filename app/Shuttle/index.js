d51791592551ba58e1e84fc067a23918fd0d7be6
keshav-Techbuilder <keshav.kumar@techbuilder.ai>
2025-03-05 11:43:03 +0530

    routes API added

--- app/Shuttle/index.js
+++ app/Shuttle/index.js
@@ -8,4 +8,24 @@ module.exports = function (app) {
     AdminMiddlewares.admin.isLoggedIn,
     shuttleRoute.insertRoute,
   );
+  app.get(
+    '/internal/fetch_routes',
+    AdminMiddlewares.admin.domainToken,
+    AdminMiddlewares.admin.isLoggedIn,
+    shuttleRoute.fetchRouteList,
+  );
+
+  app.post(
+    '/internal/edit_route',
+    AdminMiddlewares.admin.domainToken,
+    AdminMiddlewares.admin.isLoggedIn,
+    shuttleRoute.editRoute,
+  );
+
+  app.post(
+    '/internal/delete_route',
+    AdminMiddlewares.admin.domainToken,
+    AdminMiddlewares.admin.isLoggedIn,
+    shuttleRoute.deleteRoute,
+  );
 };
